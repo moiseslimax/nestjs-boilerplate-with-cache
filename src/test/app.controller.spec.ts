95715510b35eca870ee39f84754a05cb3f8cdef8
Moises Lima <moiseslimax@gmail.com>
2025-06-10 17:16:54 -0300

    adding ci workflow for github and changing api test route response

--- src/test/app.controller.spec.ts
+++ src/test/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Server status: OK"', () => {
+      expect(appController.getHello()).toBe('Server status: OK');
     });
   });
 });

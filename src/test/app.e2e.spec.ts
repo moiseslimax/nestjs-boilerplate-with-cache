4c5e5697587857036120213261dcf67d058a5119
Moises Lima <moiseslimax@gmail.com>
2025-06-19 09:25:40 -0300

    fixing tests

--- src/test/app.e2e.spec.ts
+++ src/test/app.e2e.spec.ts
@@ -23,6 +23,6 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Server status: OK!');
+      .expect('Server status: OK');
   });
 });
